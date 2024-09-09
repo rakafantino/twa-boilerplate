fca208a38679b1c7ade29ee4fdaa3a41ca7f097d
Raka Fantino <rakafantino@gmail.com>
2024-09-09 14:50:01 +0700

    first commit

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -35,9 +35,9 @@ export default function WLApp() {
   return (
     // Wrap the App component with the TwaAnalyticsProvider. Replace these credentials with your own. You can get these variables via https://docs.ton.solutions/docs/community-support
     <TwaAnalyticsProvider
-      projectId="76fbd597-d614-4506-b468-2361b0d9e419"
-      apiKey="920f7a0c-e138-4f43-a9f8-dffb5d0543f0"
-      appName="Boilerplate"
+      projectId="37b91ef5-2f16-4a60-9ee7-90599491a1d0"
+      apiKey="59ee2b23-fb5f-4d1f-91ef-b60d02bde6bf"
+      appName="twa_boilerplate"
     >
       <App theme={theme}>
         <Router>
